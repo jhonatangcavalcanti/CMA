00651e71e27d4a6368b209edc1dd2170ff3c19d4
Jhonatan Gomes Cavalcanti <jhonatan.gomes93@gmail.com>
2017-03-23 16:26:41 -0300

    feat(eslint): Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "globals": {
+    "__dirname": false,
+    "$": false,
+    "describe": false,
+    "it": false,
+    "beforeAll": false,
+    "beforeEach": false,
+    "expect": false,
+    "document": false,
+    "_": false,
+    "postMessage": false,
+    "self": false,
+    "Worker": false,
+    "dump": false,
+    "spyOn": false,
+    "jasmine": false
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "brace-style": [
+      "error",
+      "1tbs",
+      { "allowSingleLine": true }
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single",
+      { "allowTemplateLiterals": true }
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-unused-vars": [
+      "error", { "argsIgnorePattern": "next" }
+    ],
+    "no-console":0,
+    "no-var":"error",
+    "keyword-spacing": "error",
+    "space-before-blocks": "error",
+    "arrow-spacing": "error",
+    "no-path-concat": "error"
+  }
+};
