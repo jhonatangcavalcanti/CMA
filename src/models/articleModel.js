19d385df447a99987dc3668e58cb015fb01ecbfd
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-29 11:21:51 -0300

    feat(src): add virtual url to return path of an article

--- src/models/articleModel.js
+++ src/models/articleModel.js
@@ -7,4 +7,8 @@ const ArticleSchema = mongoose.Schema({
   date: { type: Date, default: Date.now }
 })
 
+ArticleSchema.virtual('url').get(() => {
+  return `/api/article/${this._id}`
+})
+
 export default mongoose.model('Article', ArticleSchema)
