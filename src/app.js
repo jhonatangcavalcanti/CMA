3be342dbe953fc600fb6e733fad470b660a0e747
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-29 11:16:13 -0300

    refactor(src): Change log to be presented only on development environment

--- src/app.js
+++ src/app.js
@@ -27,7 +27,9 @@ app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
+if (process.env.NODE_ENV == 'development') {
+  app.use(logger('dev'))
+}
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
