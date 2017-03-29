cb33ede2352a9c73616a9835502b1837381a49d6
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-29 17:33:58 -0300

    feat(routes): add controller handles to article routes

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,13 +1,10 @@
+import articleController from '../controllers/articleController'
 import express from 'express'
 const router = express.Router()
 
 /* GET articles listing. */
-router.get('/', (req, res) => {
-  res.send(`List of articles`)
-})
+router.get('/', articleController.index)
 
-router.get('/:id', (req, res) => {
-  res.send(`Article ${req.params.id}`)
-})
+router.get('/:id', articleController.article)
 
 export default router
