d1486073025a13b79001af7935880a9d550bdf56
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-29 16:11:42 -0300

    feat(models): add virtual to return formated date

--- src/models/articleModel.js
+++ src/models/articleModel.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose'
+import moment from 'moment'
 
 const ArticleSchema = mongoose.Schema({
   title: { type: String, required: true },
   image_path: { type: String },
   content: { type: String, required: true },
   date: { type: Date, default: Date.now }
+})
+
+ArticleSchema.virtual('date_formatted').get(function () {
+  return moment(this.date).format('MMMM Do, YYYY')
 })
 
 ArticleSchema.virtual('url').get(() => {
