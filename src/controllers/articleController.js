5df467f5d92d60e2d33e0cfb2468016f1abb906e
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-30 10:28:03 -0300

    feat(controller): add route handler to all artiles

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -10,4 +10,10 @@ export default class articleController {
       res.render('article', { article: JSON.parse(body) })
     })
   }
+
+  static allArticles(req, res, next) {
+    request(`http://localhost:3000/api/articles`, (err, response, body) => {
+      res.render('allArticles', { articles: JSON.parse(body) })
+    })
+  }
 }
