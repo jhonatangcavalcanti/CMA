054b963f4e49b1f6d5f6023f812a6836d23f4e71
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-03-30 10:26:56 -0300

    feat(routes): add get route to all articles

--- src/routes/article.js
+++ src/routes/article.js
@@ -5,6 +5,9 @@ const router = express.Router()
 /* GET articles listing. */
 router.get('/', articleController.index)
 
+router.get('/all', articleController.allArticles)
+
 router.get('/:id', articleController.article)
+
 
 export default router
