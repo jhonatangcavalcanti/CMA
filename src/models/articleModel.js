9d691bd5eea76e33b9cd85dc870076e06469f8ec
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-04-04 11:36:02 -0300

    feat(models): add image_id field and change virtual image_path

--- src/models/articleModel.js
+++ src/models/articleModel.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose'
 import moment from 'moment'
-import path from 'path'
 
 const ArticleSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
+  image_id: { type: String },
   date: { type: Date, default: Date.now }
 })
 
 ArticleSchema.virtual('image_path').get(() => {
-  return path.join(__dirname, '..', '..', 'images', this._id)
+  return `/article/image/${this.image_id}`
 })
 
 ArticleSchema.virtual('date_formatted').get(() => {
