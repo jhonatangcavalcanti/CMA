34caa591f2ffa520a7afc443feb1d8bbeb855b73
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-04-04 17:53:53 -0300

    feat(src): add favicon

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 import express from 'express'
 import path from 'path'
-// import favicon from 'favicon'
+import favicon from 'serve-favicon'
 import logger from 'morgan'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
@@ -26,8 +26,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'))
 app.set('views', path.join(__dirname, '..', 'views'))
 app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')))
 if (process.env.NODE_ENV == 'development') {
   app.use(logger('dev'))
 }
